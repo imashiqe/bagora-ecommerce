0186456281809c423c191930e24696ea754ac960
Ashiqur Rahman <imashiqe@gmail.com>
2026-08-18 12:28:30 +0600

    initial page added

---
+++ resources/views/frontend/pages/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('frontend.master')
+
+@section('content')
+
+<div>
+     <h1>Contact Us</h1>
+     <p>The page you are looking for does not exist.</p>
+</div>
+
+@endsection
---
+++ resources/views/frontend/pages/order-success.blade.php
@@ -0,0 +1,10 @@
+@extends('frontend.master')
+
+@section('content')
+
+<div>
+     <h1>Order Success</h1>
+     <p>The page you are looking for does not exist.</p>
+</div>
+
+@endsection
